a8f1fd2b6b17cee9fc2877ff2a1868f6746cb8eb
junwei.guo <guo_junwei@hotmail.com>
2025-08-20 21:54:38 -0600

    update build notes for the original serial summa

--- build/buildNote.sh
+++ build/buildNote.sh
@@ -27,5 +27,31 @@ make mach=anvil
 #On Fedora Linux:
 git clone --branch dev-lake-river --single-branch https://github.com/junwei-guo/summa-with-lake-river.git
 cd summa-with-lake-river/build
-
 make mach=xps
+
+# -------------------------------
+#On Anvil with the original serial SUMMA:
+
+git clone --branch dev-lake-river --single-branch https://github.com/junwei-guo/summa-with-lake-river.git
+cd summa-with-lake-river/build
+git checkout 8321632d84135f70a4d6d31e2caa34208b7b9c00
+
+## Anvil setting:
+# module --force purge
+# module load gcc  openmpi
+# module load netcdf-fortran
+# module load openblas
+
+
+F_MASTER = ../
+FC = gfortran
+
+EBROOTGCC=${GCC_HOME}
+EBROOTOPENBLAS=${OPENBLAS_HOME}
+EBROOTNETCDFMINFORTRAN=${NETCDF_FORTRAN_HOME}
+
+FC_EXE = ${EBROOTGCC}/bin/gfortran
+CC = ${EBROOTGCC}/bin/gcc
+
+INCLUDES =  -I${EBROOTOPENBLAS}/include -I${EBROOTNETCDFMINFORTRAN}/include 
+LIBRARIES = -L${EBROOTNETCDFMINFORTRAN}/lib -lnetcdff  -L${EBROOTOPENBLAS}/lib -lopenblas  
